f08e2d52cd0aa271c3771c198b470007a542343d
Sagartare12 <sagartare2014@gmail.com>
2021-05-04 14:34:52 +0530

    Add the router to browse the different page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Switch , Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-    hi
+      <Switch>
+        <Route exact path='/'>
+          Hi this is first page
+        </Route>
+        <Route exact path="/starred">
+          This is second page
+        </Route>
+        <Route>
+          Error 404 page not found
+        </Route>
+    </Switch>
     </div>
   );
 }
