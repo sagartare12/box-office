5efd2f50d6259b141465c5ae9d801e809b6a4990
Sagartare12 <sagartare2014@gmail.com>
2021-05-04 15:10:56 +0530

    Add navbar and created page compponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Switch , Route} from 'react-router-dom'
-
+import Navs from './components/Navs'
+import Home from './Pages/Home'
+import Starred from './Pages/Starred'
 function App() {
   return (
     <div className="App">
+      <Navs />
       <Switch>
         <Route exact path='/'>
-          Hi this is first page
+          <Home />
         </Route>
         <Route exact path="/starred">
-          This is second page
+          <Starred />
         </Route>
         <Route>
           Error 404 page not found
