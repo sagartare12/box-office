adf4c65cf13d5c4d860dfba9ad2a7945c5011889
Sagartare12 <sagartare2014@gmail.com>
2021-05-05 10:29:27 +0530

    Create input and fetch the api data with that input

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
  const Home = () => {
+
+    const [ input , setInput ] = useState('');
+
+
+    const onSearch = () =>{
+        // http://api.tvmaze.com/search/shows?q=girl
+        
+        fetch(`http://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result =>
+        console.log(result));
+    };
+
+    const toInputChange = (ev) => {
+        setInput(ev.target.value);
+        console.log(ev.target.value);
+    };
+
+    const onKeyDown = (ev) => {
+        if(ev.keyCode === 13 ){
+            onSearch();
+        }
+    }
+    
     return (
         <MainPageLayout>
-            This is home page
+            <input type="text" onChange={toInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch} >Search</button>
         </MainPageLayout>
     )
 }
