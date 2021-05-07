27377516e61379c2cd7292c50ebdd7b24cc32abc
Sagartare12 <sagartare2014@gmail.com>
2021-05-07 21:29:47 +0530

    Replace useState with useReducer on the show page

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,14 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
 import {useParams} from 'react-router-dom'
 
 import { apiGet } from '../misc/config';
- const Show = ( ) => {
+ 
+const reducer =( prevState , action ) => {
+    switch(action.type){
+        case 'FETCH_SUCCESS' : {
+            return { isLoading: false, error: null, show: action.show}
+        }
+        
+        case 'FETCH_FAILED' : {
+            return {...prevState, isLoading: false, error: action.error, }
+        }
+
+        default: return prevState
+    }
+
+}
+
+const initialState ={
+    show: null,
+    isLoading: true,
+    error: null,
+}
+const Show = ( ) => {
 
     const { id } = useParams();
+
+const [ { isLoading, show, error} , dispatch ] = useReducer(reducer,initialState)
+
     // console.log('params',params);
-    const [ show , setShow ] = useState(null);
-    const [ isLoading , setIsLoading] = useState(true);
-    const [ error , setError ] = useState(null);
+    // const [ show , setShow ] = useState(null);
+    // const [ isLoading , setIsLoading] = useState(true);
+    // const [ error , setError ] = useState(null);
 
     useEffect( () => {
 
@@ -17,14 +41,18 @@ import { apiGet } from '../misc/config';
         apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then( results => {
        
             if(isMounted){
-            setShow(results);
-            setIsLoading(false);
+            // setShow(results);
+            // setIsLoading(false);
+
+            dispatch( { type: 'FETCH_SUCCESS' , show: results})
             }
                   
         }).catch( err => {
             if(isMounted){
-                setError(err.message);
-            setIsLoading(false);
+            // setError(err.message);
+            // setIsLoading(false);
+
+            dispatch( { type: 'FETCH_FAILED' , error: err.message })
             }
         });
         return () => {
