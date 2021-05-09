575c9127e824f813d9bf85190e6adf7aae8ead2a
Sagartare12 <sagartare2014@gmail.com>
2021-05-09 17:39:39 +0530

    Create two new more hooks useShow and lastQuery in custom-hooks file

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,69 +1,20 @@
-import React, { useEffect,  useReducer } from 'react'
+import React from 'react'
 import {useParams} from 'react-router-dom'
 import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
 
-import { apiGet } from '../misc/config';
+
 import { InfoBlock, ShowPageWrapper } from './Show.styled';
- 
-const reducer =( prevState , action ) => {
-    switch(action.type){
-        case 'FETCH_SUCCESS' : {
-            return { isLoading: false, error: null, show: action.show}
-        }
-        
-        case 'FETCH_FAILED' : {
-            return {...prevState, isLoading: false, error: action.error, }
-        }
+import { useShow } from '../misc/custom-hooks'
 
-        default: return prevState
-    }
 
-}
-
-const initialState ={
-    show: null,
-    isLoading: true,
-    error: null,
-}
 const Show = ( ) => {
 
     const { id } = useParams();
+    const { show, isLoading, error } = useShow(id);
 
-const [ { isLoading, show, error} , dispatch ] = useReducer(reducer,initialState)
-
-    // console.log('params',params);
-    // const [ show , setShow ] = useState(null);
-    // const [ isLoading , setIsLoading] = useState(true);
-    // const [ error , setError ] = useState(null);
-
-    useEffect( () => {
-
-        let isMounted = true;
-
-        apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then( results => {
-       
-            if(isMounted){
-            // setShow(results);
-            // setIsLoading(false);
-
-            dispatch( { type: 'FETCH_SUCCESS' , show: results})
-            }
-                  
-        }).catch( err => {
-            if(isMounted){
-            // setError(err.message);
-            // setIsLoading(false);
-
-            dispatch( { type: 'FETCH_FAILED' , error: err.message })
-            }
-        });
-        return () => {
-            isMounted =false;
-        };
-    },[id])
     console.log( 'show',show);
 
     if(isLoading){
